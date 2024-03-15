f21a63c403a423c7e66fe168b846fcd70ac2b650
tan2000529 <tancheehonghong4@gmail.com>
2024-03-15 15:23:30 +0800

    gg

---
+++ Content/StudentManageAccount.php
@@ -0,0 +1,34 @@
+<?php
+include('../Header/head.php');
+include('../Header/header.html');
+?>
+<div class="container">
+<form>
+<div class="form-group row">
+    <label class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" placeholder="Name">
+    </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Update</button>
+    </div>
+  </div>
+</form>
+</div>
