a9e7245925f1d192e19f1cb38cd659890e50e1ae
tan2000529 <tancheehonghong4@gmail.com>
2024-04-10 08:19:36 +0800

    check email before update

--- Content/lecturer_profile_request.php
+++ Content/lecturer_profile_request.php
@@ -7,50 +7,54 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
    $Email=$_POST['Email'];
    $Password=$_POST['Password'];
    $Repeat_Password=$_POST['Repeat_Password'];
-// SQL query to check if the email exists for other lecturers
-$sqlSelectEmail = "SELECT COUNT(*) AS emailCount FROM lecturer WHERE Email = '$Email' AND Lecturer_Id != '$Lecturer_Id'";
 
-// Execute the query
-$result = mysqli_query($conn, $sqlSelectEmail);
 
-// Check if the query was successful
-if ($result) {
-    // Fetch the result
-    $row = mysqli_fetch_assoc($result);
-    // here need to check the email for both
-    
-    // Check if the email count is greater than or equal to 1
-    if ($row['emailCount'] >= 1) {
-        header("location:lecturer_profile.php?error=Update Fail due to email have been used by other lecturer!");
-    exit;
-    }
-} 
-   if(empty($Password)&&empty($Repeat_Password)){
-    
-    $sqlUpdatelecturerInformation="UPDATE lecturer
-   SET Lecturer_Name = '$Name', Email = '$Email'
-   WHERE Lecturer_Id='$Lecturer_Id'";
-    
-    $conn->query($sqlUpdatelecturerInformation);
-    
-    header("location:lecturer_profile.php?success=Updated lecturer profile successfull!");
+   $sqlCheckEmail = "SELECT Id, Email, LoginId
+   FROM (
+       (SELECT student.Student_Id AS Id, student.Email, student.Student_Login_Id AS LoginId
+       FROM student 
+       JOIN program ON program.Program_Id = student.Program_Id)
+       UNION ALL
+       (SELECT lecturer.Lecturer_Id AS Id, lecturer.Email, lecturer.Lecturer_Login_Id AS LoginId
+       FROM lecturer)
+   ) AS combined_data
+   WHERE Email='$Email' AND Id!='$Lecturer_Id'";
+   
 
-   }else{
-            if($Password==$Repeat_Password) {
-    $Password = password_hash($Password, PASSWORD_DEFAULT);
-   $sqlUpdatelecturerInformation="UPDATE lecturer
-   SET Lecturer_Name = '$Name', Email = '$Email',Lecturer_Password='$Password'
-   WHERE Lecturer_Id='$Lecturer_Id'";
-    
-    $conn->query($sqlUpdatelecturerInformation);
-    header("location:lecturer_profile.php?success=Updated lecturer profile successfull!");
+if(mysqli_num_rows(mysqli_query($conn,$sqlCheckEmail))>=1){
+// the id or the email have been used
+header("location:lecturer_profile.php?error=Register fail due to the the email have been used.");
 
-// $stmt->close();
-    $conn->close();
-            }else{
-                header("location:lecturer_profile.php?error=Updated lecturer profile fail due to the password and repeat does not match!");
+}else{
+
+    if(empty($Password)&&empty($Repeat_Password)){
+    
+        $sqlUpdatelecturerInformation="UPDATE lecturer
+       SET Lecturer_Name = '$Name', Email = '$Email'
+       WHERE Lecturer_Id='$Lecturer_Id'";
+        
+        $conn->query($sqlUpdatelecturerInformation);
+        
+        header("location:lecturer_profile.php?success=Updated lecturer profile successfull!");
+    
+       }else{
+                if($Password==$Repeat_Password) {
+        $Password = password_hash($Password, PASSWORD_DEFAULT);
+       $sqlUpdatelecturerInformation="UPDATE lecturer
+       SET Lecturer_Name = '$Name', Email = '$Email',Lecturer_Password='$Password'
+       WHERE Lecturer_Id='$Lecturer_Id'";
+        
+        $conn->query($sqlUpdatelecturerInformation);
+        header("location:lecturer_profile.php?success=Updated lecturer profile successfull!");
+    
+    // $stmt->close();
+        $conn->close();
+                }else{
+                    header("location:lecturer_profile.php?error=Updated lecturer profile fail due to the password and repeat does not match!");
+                }
             }
-        }
+}
+   
 }
 
 ?>
