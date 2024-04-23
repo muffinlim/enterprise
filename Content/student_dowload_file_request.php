1cdb44ca7326a02627d3ce9b2587632e037f0083
muffinlim <93931326+muffinlim@users.noreply.github.com>
2024-04-23 10:52:25 +0800

    test file upload

--- Content/student_dowload_file_request.php
+++ Content/student_dowload_file_request.php
@@ -3,7 +3,8 @@
 if(!empty($_GET['File_Link'])){
     // get the file name
 $File_Link=basename($_GET['File_Link']);
-$path_store="C:/xampp/htdocs/GitHub/enterprise/download_upload_file/".$File_Link;
+// $path_store="C:/xampp/htdocs/GitHub/enterprise/download_upload_file/".$File_Link;
+$path_store ="C:/xampp/htdocs/enterprise/download_upload_file/".$File_Link;
 if(!empty($File_Link)&&file_exists($path_store)){
 
          header("Cache-Control: public");
